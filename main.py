21e5bb41e5d642da1910922fd4f313bf68930c29
prazol09 <prazolbasnet11@gmail.com>
2022-03-04 14:07:13 +0545

    little more functions added

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 # a functon to make function
 #
 
+def parm_taker():
+    parameter = input("Enter a parameter name: ")
+    print("Parameter successfully set!")
+    return parameter
+
+
+def basicfn():
+
+
 def functioner():
     is_parametarized = input("Will this function be paremarized?y/n: ")
     is_p = is_parametarized.upper()
     if (is_p == "Y"):
-        print('make a pparametarized function')
+        #print('make a parametarized function')
+        int nop = int(input("Enter the number of parameters: "))
+        parameters = []
+        for i in range(nop):
+            
     elif (is_p == "N"):
         print("make a nonprematerixed fuction.")
     else:
